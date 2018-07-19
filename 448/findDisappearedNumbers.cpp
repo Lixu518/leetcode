689823e4b2aef0d9b758769e360df9820f5a36a3
Lixu518 <lx518@bupt.edu.cn>
2018-07-19 19:27:13 +0800

    7-19

---
+++ 448/findDisappearedNumbers.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+vector<int> findDisappearedNumbers(vector<int>& nums) {
+	vector<int>res;
+	int i = 0;
+	while(i < nums.size()){
+		if(nums[i] != nums[nums[i] - 1]) swap(nums[i], nums[nums[i] - 1]);
+		else i++;
+	}
+	for(int i = 0;i < nums.size();i++){
+		if(nums[i] != i + 1)res.push_back(i+1);
+	}
+	return res;
+}
+
+int main(){
+	vector<int>nums = {4,3,2,7,8,2,3,1};
+	vector<int>res = findDisappearedNumbers(nums);
+	for(auto num :res)
+		cout<<num<<" ";
+	cout<<endl;
+	return 0;
+}
