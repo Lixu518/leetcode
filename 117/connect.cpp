f0e903119fcdf4a9a543b7546403821820010259
Lixu518 <lx518@bupt.edu.cn>
2018-08-07 00:14:45 +0800

    8-6

---
+++ 117/connect.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+struct TreeLinkNode{
+    int val;
+    TreeLinkNode *left, *right, *next;
+    TreeLinkNode(int x):val(x), left(NULL), right(NULL),next(NULL){}
+}
+
+void connnect(TreeLinkNode* root){
+    TreeLinkNode* dummy = new TreeLinkNode(0), *t = dummy;
+    while(root){
+        if(root->left){
+            t->next = root->left;
+            t = t->next;
+        }
+        if(root->right){
+            t->next = root->right;
+            t = t->next;
+        }
+        root = root->next;
+        if(!root){
+            t = dummy;
+            root = dummy->next;
+            dummy->next = NULL;
+        }
+    }
+}
