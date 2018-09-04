083ef45b983702e1a58fad1fc40e333fe2220033
Lixu518 <lx518@bupt.edu.cn>
2018-09-05 00:18:07 +0800

    9-4

---
+++ 218/getSkyline.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<utility>
+#include<set>
+#include<algorithm>
+using namespace std;
+vector<pair<int, int>> getSkyline(vector<vector<int>> &buildings){
+	vector<pair<int, int>> h, res;
+	multiset<int> m;
+	int pre = 0, cur = 0;
+	for (auto &a : buildings) {
+		h.push_back({a[0], -a[2]});
+		h.push_back({a[1], a[2]});
+	}
+	sort(h.begin(), h.end());
+	m.insert(0);
+	for (auto &a : h) {
+		if (a.second < 0) m.insert(-a.second);
+		else m.erase(m.find(a.second));
+		cur = *m.rbegin();
+		if (cur != pre) {
+			res.push_back({a.first, cur});
+			pre = cur;
+		}
+	}
+	return res;
+}
+int main(){
+    vector<vector<int>> buildings = { {2, 9, 10}, {3, 7, 15}, {5, 12, 12}, {15,
+        20, 10},{19, 24, 8} };
+    vector<pair<int, int>> res;
+    res = getSkyline(buildings);
+    for(auto &a : res){
+        cout<<a.first<<" "<<a.second<<endl;
+    }
+    return 0;
+}
